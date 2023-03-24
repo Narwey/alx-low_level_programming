0ea6711ca30bc8e5077428c73fcff1e01ad2aed0
ANOUAR BENMBARK <benmbark.anouar@gmail.com>
2023-03-23 19:39:34 -0700

    print_most_number

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,17 @@
  */
 void print_most_numbers(void)
 {
-	int c;
+int c;
 
-	for (c = 48; c < 58; c++)
+for (c = 48; c < 58; c++)
 {
-		if (c != 50 || c != 52)
-	{
-		_putchar(c);
-	}
+	if (c != 50)
+{
+	if (c != 52)
+{
+	_putchar(c);
 }
-		_putchar('\n');
+}
+}
+	_putchar('\n');
 }
